217e8bdae10cb507cb15c69ecbca50c34f6063b6
Nemanja369 <nemanjamilosevic01@gmail.com>
2021-10-18 01:32:41 +0100

    new commit

--- src/service.js
+++ src/service.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const getAllUsers = () => axios.get(`http://localhost:3005/users`)
-export const postUser = (username,email,password) => axios.post(`http://localhost:3005/users`,{username,email,password})
-export const getAllParts = () => axios.get(`http://localhost:3005/parts`)
-export const getPartById = (id) => axios.get(`http://localhost:3005/parts/${id}`)
-export const addPart = (name,material, code, machine) => axios.post(`http://localhost:3005/parts`,{name,material,code, machine})
-export const deletePart = (id) => axios.delete(`http://localhost:3005/parts/${id}`)
-export const editPart = (id,name,material,code, machine) =>axios.put(`http://localhost:3005/parts/${id}`, {name,material,code, machine})
+export const getAllUsers = () => axios.get(`https://finalproject-orox.herokuapp.com/users`)
+export const postUser = (username,email,password) => axios.post(`https://finalproject-orox.herokuapp.com/users`,{username,email,password})
+export const getAllParts = () => axios.get(`https://finalproject-orox.herokuapp.com/parts`)
+export const getPartById = (id) => axios.get(`https://finalproject-orox.herokuapp.com/parts/${id}`)
+export const addPart = (name,material, code, machine) => axios.post(`https://finalproject-orox.herokuapp.com/parts`,{name,material,code, machine})
+export const deletePart = (id) => axios.delete(`https://finalproject-orox.herokuapp.com/parts/${id}`)
+export const editPart = (id,name,material,code, machine) =>axios.put(`https://finalproject-orox.herokuapp.com/parts/${id}`, {name,material,code, machine})
