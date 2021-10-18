f6cda429b1bbda43c3360bbb757e9490bef5bdfc
Nemanja369 <nemanjamilosevic01@gmail.com>
2021-10-18 01:06:55 +0100

    komit

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -7,7 +7,7 @@ const Search = ({ setInputSearch, inputSearch }) => {
                 <div className="col-10 offset-1">
                     <div className="form-group row">
                         <input type="search" className="form-control" value={inputSearch} placeholder="search..."
-                               onChange={(e) => {setInputSearch(e.target.value)}} />
+                               git onChange={(e) => {setInputSearch(e.target.value)}} />
                     </div>
                 </div>
             </div>
